b60452e9b842ac40f2bc7b7b5e042c53cfd34b66
Ed <eduardme@ya.ru>
2022-07-15 06:33:36 +0500

    all files with questionnaires are moved to a separate directory

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
 # CONFIGURATION FILE FOR PSY-TEST-BOT
 
-LANGUAGE = "RU" # RU | EN supported
+LANGUAGE = "RU"  # RU | EN supported
 
 MAX_USERS = 10
 
-MAX_TIME = 900
+MAX_TIME = 900  # in sec.
+
+MAX_SESSION_TIME = 1800  # in sec.
+
+MAX_IDLE_TIME = 420  # in sec.
+
+TESTS_DIR = "tests"  # a directory where tests (questionnaires) are stored
+
+TEST_EXTN = "txt"  # files' extension for files with tests (questionnaires)
